a0c169482e3b5bfe2fe3df4c99b61ec8766a940a
pa.solodov <pa.solodov@tensor.ru>
2018-12-03 17:22:09 +0300

    back shuffle method, add shuffle result validation with reshuffle

--- file-shuffle-helper.js
+++ file-shuffle-helper.js
@@ -25,20 +25,79 @@ function getNumbersArr(length) {
       arr[i] = (prefix + (i + 1)).match(RegExp('\\d{' + numOfDigits + '}$')) + '';
    }
 
-   return arr.sort(function(a,b){
-      return Math.random() - 0.5;
-   });
+   return arr;
+}
+
+function shuffle(arr) {
+   var j, x, i;
+   for (i = arr.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = arr[i];
+      arr[i] = arr[j];
+      arr[j] = x;
+   }
+   return arr;
+}
+
+function getChangesStat(shuffledArr) {
+   var areas = [],
+      totalAreasAmount = 0,
+      samePositions = [],
+      maxAreaLength = 0,
+      curArea,
+      continueArea = false,
+      isArea = false;
+   for (var i = 0, len = shuffledArr.length; i < len; i++) {
+      continueArea = i < len-1 && +shuffledArr[i+1] - +shuffledArr[i] === 1;
+      if (continueArea) {
+         if (!areas.length || !isArea) {
+            curArea = [];
+            areas.push(curArea);
+            isArea = true;
+         }
+      }
+      if (isArea) {
+         curArea.push(i);
+         if(curArea.length > maxAreaLength) {
+            maxAreaLength = curArea.length;
+         }
+         totalAreasAmount++;
+         isArea = continueArea
+      }
+      if (+shuffledArr[i] === i+1) {
+         samePositions.push(i);
+      }
+   }
+
+   return {
+      areas: areas,
+      areasAmount: totalAreasAmount,
+      areasMaxLength: maxAreaLength,
+      samePosElements: samePositions
+   }
+}
+
+function validateShuffleResult(shuffledArr) {
+   var len = shuffledArr.length,
+      stat = getChangesStat(shuffledArr),
+      percentOfRepetedAreas = len ? stat.areasAmount / len : 1;
+   return percentOfRepetedAreas < 0.05 && stat.areasMaxLength < 3
 }
 
 function getNewFileName(index, name) {
-   return newNumbers[index] + ' - ' + name.replace(/^\d* - /, '');
+   return shuffledNumbers[index] + ' - ' + name.replace(/^\d* - /, '');
 }
 
 var filesBuffer = fs.readdirSync(pathToFolder),
    filesCount = filesBuffer && filesBuffer.length || 0,
    fileMaskRegExp = getFileMaskRegExp(fileExt),
-   newNumbers = filesCount ? getNumbersArr(filesCount) : 0,
+   numbers = filesCount ? getNumbersArr(filesCount) : 0,
+   shuffledNumbers = shuffle(numbers.slice()),
    curFileName;
+
+while (!validateShuffleResult(shuffledNumbers)) {
+   shuffle(shuffledNumbers);
+}
 
 for (var i = 0; i < filesCount; i++) {
    curFileName = filesBuffer[i] || '';
